2920f041756dea5a356b0c9b3fbfea18988943e9
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-08-15 14:44:33 +0800

    expectToFail, regex for expectToFailWith

--- src/index.ts
+++ src/index.ts
@@ -38,8 +38,13 @@ export function tsst(scope: () => void | ReadonlyArray<Error>): TsstResult {
         expectToCompile() {
             if (this.errors.length) throw this.errors[0];
         },
-        expectToFailWith(msg: string) {
-            if (this.errors.filter(e => e.message === msg).length === 0) {
+        expectToFail() {
+            if (!this.errors.length) {
+                throw new Error("No semantic failures");
+            }
+        },
+        expectToFailWith(msg: string | RegExp) {
+            if (this.errors.filter(e => RegExp(msg).test(e.message)).length === 0) {
                 throw new Error("No matching semantic failures");
             }
         }
@@ -50,5 +55,6 @@ export interface TsstResult {
     readonly errors: ReadonlyArray<Error>;
     // These are just placeholders, full set of proper expectations coming
     expectToCompile(): void;
-    expectToFailWith(msg: string): void;
+    expectToFail(): void;
+    expectToFailWith(msg: string | RegExp): void;
 }
